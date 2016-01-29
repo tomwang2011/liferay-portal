bf8bc7d2e10289d2bc0d96ad2c407753626d502b
tomwang2011 <tong.wang@liferay.com>
2016-01-29 11:16:56 -0800

    LPS-61952 Manually change portal-impl imports

--- portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.model.impl;
 
+import com.liferay.expando.kernel.model.CustomAttributesDisplay;
 import com.liferay.portal.kernel.application.type.ApplicationType;
 import com.liferay.portal.kernel.atom.AtomCollectionAdapter;
 import com.liferay.portal.kernel.log.Log;
@@ -66,7 +67,6 @@ import com.liferay.portlet.ControlPanelEntry;
 import com.liferay.portlet.DefaultControlPanelEntryFactory;
 import com.liferay.portlet.PortletQNameUtil;
 import com.liferay.portlet.asset.model.AssetRendererFactory;
-import com.liferay.portlet.expando.model.CustomAttributesDisplay;
 import com.liferay.portlet.exportimport.lar.PortletDataHandler;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandler;
 import com.liferay.portlet.social.model.SocialActivityInterpreter;
