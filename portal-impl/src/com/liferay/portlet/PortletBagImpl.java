a1bb9d00743cece7e5da04d558450e78fe3fe03e
tomwang2011 <tong.wang@liferay.com>
2016-01-29 11:16:56 -0800

    LPS-61952 Manually change portal-impl imports

--- portal-impl/src/com/liferay/portlet/PortletBagImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletBagImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet;
 
+import com.liferay.expando.kernel.model.CustomAttributesDisplay;
 import com.liferay.portal.kernel.atom.AtomCollectionAdapter;
 import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
 import com.liferay.portal.kernel.notifications.UserNotificationHandler;
@@ -37,7 +38,6 @@ import com.liferay.portal.kernel.webdav.WebDAVStorage;
 import com.liferay.portal.kernel.workflow.WorkflowHandler;
 import com.liferay.portal.kernel.xmlrpc.Method;
 import com.liferay.portlet.asset.model.AssetRendererFactory;
-import com.liferay.portlet.expando.model.CustomAttributesDisplay;
 import com.liferay.portlet.exportimport.lar.PortletDataHandler;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandler;
 import com.liferay.portlet.social.model.SocialActivityInterpreter;
