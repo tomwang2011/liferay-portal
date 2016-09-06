2b0af0d48df5921995597fcc6e83fd4cd0b1abfa
Preston-Crary <preston.crary@liferay.com>
2016-09-06 12:07:46 -0700

    Add arrayable-pagination option to service builder, it can only be used in the case where we are most likely to overflow a SQL in cause.

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -5231,6 +5231,8 @@ public class ServiceBuilder {
 					finderColumnElement.attributeValue("comparator"), "=");
 				String finderColArrayableOperator = GetterUtil.getString(
 					finderColumnElement.attributeValue("arrayable-operator"));
+				boolean finderColArrayablePagination = GetterUtil.getBoolean(
+					finderColumnElement.attributeValue("arrayable-pagination"));
 
 				EntityColumn col = Entity.getColumn(finderColName, columnList);
 
@@ -5243,6 +5245,7 @@ public class ServiceBuilder {
 				col.setCaseSensitive(finderColCaseSensitive);
 				col.setComparator(finderColComparator);
 				col.setArrayableOperator(finderColArrayableOperator);
+				col.setArrayablePagination(finderColArrayablePagination);
 
 				col.validate();
 
