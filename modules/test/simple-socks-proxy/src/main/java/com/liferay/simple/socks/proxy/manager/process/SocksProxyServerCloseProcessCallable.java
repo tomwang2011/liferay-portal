b95762ca4448ef4d624b74c3287d43568785fbdc
tomwang2011 <tong.wang@liferay.com>
2018-07-16 11:30:11 -0700

    LPS-74610 Add sources

---
+++ modules/test/simple-socks-proxy/src/main/java/com/liferay/simple/socks/proxy/manager/process/SocksProxyServerCloseProcessCallable.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.simple.socks.proxy.manager.process;
+
+import com.liferay.petra.process.ProcessCallable;
+import com.liferay.petra.process.ProcessException;
+import com.liferay.petra.process.local.LocalProcessLauncher;
+import com.liferay.simple.socks.proxy.manager.process.server.SocksProxyServer;
+
+import java.io.Serializable;
+
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @author Tom Wang
+ */
+public class SocksProxyServerCloseProcessCallable
+	implements ProcessCallable<Serializable> {
+
+	@Override
+	public Serializable call() throws ProcessException {
+		ConcurrentMap<String, Object> attributes =
+			LocalProcessLauncher.ProcessContext.getAttributes();
+
+		SocksProxyServer socksProxyServer = (SocksProxyServer)attributes.remove(
+			SocksProxyServer.class.getName());
+
+		if (socksProxyServer != null) {
+			try {
+				socksProxyServer.close();
+			}
+			catch (Exception e) {
+				throw new ProcessException(e);
+			}
+		}
+
+		return 0;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
