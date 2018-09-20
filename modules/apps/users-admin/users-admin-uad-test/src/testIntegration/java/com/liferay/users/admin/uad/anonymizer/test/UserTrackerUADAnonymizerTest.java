a1574ac9998e6613cc12db8d4d62a3b5da127951
tomwang2011 <tong.wang@liferay.com>
2018-09-20 10:01:32 -0700

    LPS-85556 Refactor UserTrackerUADTestHelper to avoid component self registration

--- modules/apps/users-admin/users-admin-uad-test/src/testIntegration/java/com/liferay/users/admin/uad/anonymizer/test/UserTrackerUADAnonymizerTest.java
+++ modules/apps/users-admin/users-admin-uad-test/src/testIntegration/java/com/liferay/users/admin/uad/anonymizer/test/UserTrackerUADAnonymizerTest.java
@@ -15,7 +15,6 @@
 package com.liferay.users.admin.uad.anonymizer.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserTracker;
 import com.liferay.portal.kernel.service.UserTrackerLocalService;
@@ -23,35 +22,28 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 import com.liferay.user.associated.data.test.util.BaseUADAnonymizerTestCase;
-
 import com.liferay.users.admin.uad.test.UserTrackerUADTestHelper;
-
-import org.junit.After;
-import org.junit.ClassRule;
-import org.junit.Rule;
-
-import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
 /**
  * @author Brian Wing Shun Chan
- * @generated
  */
 @RunWith(Arquillian.class)
-public class UserTrackerUADAnonymizerTest extends BaseUADAnonymizerTestCase<UserTracker> {
+public class UserTrackerUADAnonymizerTest
+	extends BaseUADAnonymizerTestCase<UserTracker> {
+
 	@ClassRule
 	@Rule
-	public static final AggregateTestRule aggregateTestRule = new LiferayIntegrationTestRule();
-
-	@After
-	public void tearDown() throws Exception {
-		_userTrackerUADTestHelper.cleanUpDependencies(_userTrackers);
-	}
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
 
 	@Override
 	protected UserTracker addBaseModel(long userId) throws Exception {
@@ -61,19 +53,15 @@ public class UserTrackerUADAnonymizerTest extends BaseUADAnonymizerTestCase<User
 	@Override
 	protected UserTracker addBaseModel(long userId, boolean deleteAfterTestRun)
 		throws Exception {
-		UserTracker userTracker = _userTrackerUADTestHelper.addUserTracker(userId);
+
+		UserTracker userTracker = UserTrackerUADTestHelper.addUserTracker(
+			_userTrackerLocalService, userId);
 
 		if (deleteAfterTestRun) {
 			_userTrackers.add(userTracker);
 		}
 
 		return userTracker;
-	}
-
-	@Override
-	protected void deleteBaseModels(List<UserTracker> baseModels)
-		throws Exception {
-		_userTrackerUADTestHelper.cleanUpDependencies(baseModels);
 	}
 
 	@Override
@@ -84,6 +72,7 @@ public class UserTrackerUADAnonymizerTest extends BaseUADAnonymizerTestCase<User
 	@Override
 	protected boolean isBaseModelAutoAnonymized(long baseModelPK, User user)
 		throws Exception {
+
 		return isBaseModelDeleted(baseModelPK);
 	}
 
@@ -96,12 +85,13 @@ public class UserTrackerUADAnonymizerTest extends BaseUADAnonymizerTestCase<User
 		return false;
 	}
 
-	@DeleteAfterTestRun
-	private final List<UserTracker> _userTrackers = new ArrayList<UserTracker>();
-	@Inject
-	private UserTrackerLocalService _userTrackerLocalService;
-	@Inject
-	private UserTrackerUADTestHelper _userTrackerUADTestHelper;
 	@Inject(filter = "component.name=*.UserTrackerUADAnonymizer")
 	private UADAnonymizer _uadAnonymizer;
+
+	@Inject
+	private UserTrackerLocalService _userTrackerLocalService;
+
+	@DeleteAfterTestRun
+	private final List<UserTracker> _userTrackers = new ArrayList<>();
+
 }
