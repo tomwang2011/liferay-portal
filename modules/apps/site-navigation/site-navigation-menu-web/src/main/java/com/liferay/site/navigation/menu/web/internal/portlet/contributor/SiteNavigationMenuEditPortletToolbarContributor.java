f5ce454d7a55b698d645431d6e5f37adcc9d401a
tomwang2011 <tong.wang@liferay.com>
2018-09-21 16:56:47 -0700

    LPS-85611 SiteNavigationMenuPortletToolbarContributor does not need to declare itself as service

--- modules/apps/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/portlet/contributor/SiteNavigationMenuEditPortletToolbarContributor.java
+++ modules/apps/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/portlet/contributor/SiteNavigationMenuEditPortletToolbarContributor.java
@@ -53,10 +53,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.name=" + SiteNavigationMenuPortletKeys.SITE_NAVIGATION_MENU,
 		"mvc.path=-", "mvc.path=/view.jsp"
 	},
-	service = {
-		PortletToolbarContributor.class,
-		SiteNavigationMenuEditPortletToolbarContributor.class
-	}
+	service = PortletToolbarContributor.class
 )
 public class SiteNavigationMenuEditPortletToolbarContributor
 	implements PortletToolbarContributor {
